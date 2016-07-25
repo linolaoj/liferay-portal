80270fb6df0dfb3173f850286d3502a7aa37abca
Rodrigo Paulino <rodrigo.paulino@liferay.com>
2016-07-25 18:16:52 -0300

    PoC: Facet Filter Classic

---
+++ modules/apps/foundation/portal-search/portal-search-web/src/main/java/com/liferay/portal/search/web/internal/search/facet/portlet/SearchFacetDisplayContext.java
@@ -0,0 +1,47 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.search.web.internal.search.facet.portlet;
+
+import com.liferay.portal.search.web.internal.results.data.SearchResultsData;
+
+import java.io.Serializable;
+
+/**
+ * @author André de Oliveira
+ * @author Rodrigo Paulino
+ */
+public class SearchFacetDisplayContext implements Serializable {
+
+	public static final String ATTRIBUTE = "SearchFacetDisplayContext";
+
+	public SearchFacetDisplayContext(
+		String qParameter, SearchResultsData searchResultsData) {
+
+		_qParameter = qParameter;
+		_searchResultsData = searchResultsData;
+	}
+
+	public String getQ() {
+		return _qParameter;
+	}
+
+	public SearchResultsData getSearchResultsData() {
+		return _searchResultsData;
+	}
+
+	private final String _qParameter;
+	private final SearchResultsData _searchResultsData;
+
+}
