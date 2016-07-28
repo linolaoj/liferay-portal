36185075e419dbccb427fe58a3139ed885a5258f
Kevin Tan <kevin.tan@liferay.com>
2016-07-27 18:13:01 -0700

    Search Bar Portlet

---
+++ modules/apps/foundation/portal-search/portal-search-web/src/main/java/com/liferay/portal/search/web/internal/portlet/bar/SearchBarRedirectMVCActionCommand.java
@@ -0,0 +1,131 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.search.web.internal.portlet.bar;
+
+import com.liferay.portal.kernel.model.Layout;
+import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCActionCommand;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
+import com.liferay.portal.kernel.servlet.SessionErrors;
+import com.liferay.portal.kernel.servlet.SessionMessages;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.CharPool;
+import com.liferay.portal.kernel.util.HttpUtil;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.search.web.internal.display.context.PortletRequestThemeDisplaySupplier;
+import com.liferay.portal.search.web.internal.display.context.ThemeDisplaySupplier;
+
+import java.util.Optional;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author André de Oliveira
+ */
+@Component(
+	property = {
+		"javax.portlet.name=" + SearchBarPortletKeys.PORTLET_NAME,
+		"mvc.command.name=redirectSearchBar"
+	},
+	service = MVCActionCommand.class
+)
+public class SearchBarRedirectMVCActionCommand extends BaseMVCActionCommand {
+
+	protected String addQParameter(
+		String url, ActionRequest actionRequest,
+		SearchBarPortletPreferences searchBarPortletPreferences) {
+
+		String keywordsParameterNameString =
+			searchBarPortletPreferences.getKeywordsParameterNameString();
+
+		String q = ParamUtil.getString(
+			actionRequest, keywordsParameterNameString);
+
+		return HttpUtil.addParameter(url, keywordsParameterNameString, q);
+	}
+
+	@Override
+	protected void doProcessAction(
+			ActionRequest actionRequest, ActionResponse actionResponse)
+		throws Exception {
+
+		if (SessionErrors.isEmpty(actionRequest)) {
+			SessionMessages.add(
+				actionRequest, portal.getPortletId(actionRequest),
+				SessionMessages.KEY_SUFFIX_HIDE_DEFAULT_SUCCESS_MESSAGE);
+		}
+
+		SearchBarPortletPreferences searchBarPortletPreferences =
+			new SearchBarPortletPreferencesImpl(
+				Optional.ofNullable(actionRequest.getPreferences()));
+
+		String redirectURL = getRedirectURL(
+			actionRequest, searchBarPortletPreferences);
+
+		redirectURL = addQParameter(
+			redirectURL, actionRequest, searchBarPortletPreferences);
+
+		actionResponse.sendRedirect(portal.escapeRedirect(redirectURL));
+	}
+
+	protected String getFriendlyURL(ThemeDisplay themeDisplay) {
+		Layout layout = themeDisplay.getLayout();
+
+		return layout.getFriendlyURL(themeDisplay.getLocale());
+	}
+
+	protected String getRedirectURL(
+		ActionRequest actionRequest,
+		SearchBarPortletPreferences searchBarPortletPreferences) {
+
+		ThemeDisplay themeDisplay = getThemeDisplay(actionRequest);
+
+		String url = themeDisplay.getURLCurrent();
+
+		String page = getFriendlyURL(themeDisplay);
+
+		String path = url.substring(0, url.indexOf(page));
+
+		Optional<String> destinationOptional =
+			searchBarPortletPreferences.getDestination();
+
+		String destination = destinationOptional.orElse(page);
+
+		return path + '/' + unslash(destination);
+	}
+
+	protected ThemeDisplay getThemeDisplay(ActionRequest actionRequest) {
+		ThemeDisplaySupplier themeDisplaySupplier =
+			new PortletRequestThemeDisplaySupplier(actionRequest);
+
+		return themeDisplaySupplier.getThemeDisplay();
+	}
+
+	protected String unslash(String string) {
+		if (string.charAt(0) == CharPool.SLASH) {
+			return string.substring(1);
+		}
+
+		return string;
+	}
+
+	@Reference
+	protected Portal portal;
+
+}
