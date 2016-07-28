97851f41a2e27f39e28cffcaa197bcaa647feeaf
Kevin Tan <kevin.tan@liferay.com>
2016-07-27 18:13:01 -0700

    Search Bar Portlet

---
+++ modules/apps/foundation/portal-search/portal-search-web/src/main/java/com/liferay/portal/search/web/internal/portlet/bar/SearchBarPortlet.java
@@ -0,0 +1,149 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.search.web.internal.portlet.bar;
+
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.search.web.internal.display.context.SearchScope;
+import com.liferay.portal.search.web.portlet.shared.search.PortletSharedSearchRequest;
+import com.liferay.portal.search.web.portlet.shared.search.PortletSharedSearchResponse;
+import com.liferay.portal.search.web.portlet.shared.search.PortletSharedSearchSettings;
+import com.liferay.portal.search.web.portlet.shared.search.SearchAwarePortlet;
+
+import java.io.IOException;
+
+import java.util.Optional;
+
+import javax.portlet.Portlet;
+import javax.portlet.PortletException;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author André de Oliveira
+ */
+@Component(
+	immediate = true,
+	property = {
+		"com.liferay.portlet.add-default-resource=true",
+		"com.liferay.portlet.css-class-wrapper=" +
+			SearchBarPortletKeys.CSS_CLASS_WRAPPER,
+		"com.liferay.portlet.display-category=category.search",
+		"com.liferay.portlet.header-portlet-css=" +
+			SearchBarPortletKeys.HEADER_PORTLET_CSS,
+		"com.liferay.portlet.icon=/icons/search.png",
+		"com.liferay.portlet.instanceable=true",
+		"com.liferay.portlet.layout-cacheable=true",
+		"com.liferay.portlet.preferences-owned-by-group=true",
+		"com.liferay.portlet.private-request-attributes=false",
+		"com.liferay.portlet.private-session-attributes=false",
+		"com.liferay.portlet.restore-current-view=false",
+		"com.liferay.portlet.use-default-template=true",
+		"javax.portlet.display-name=" + SearchBarPortletKeys.DISPLAY_NAME,
+		"javax.portlet.expiration-cache=0",
+		"javax.portlet.init-param.template-path=/",
+		"javax.portlet.init-param.view-template=" +
+			SearchBarPortletKeys.VIEW_TEMPLATE,
+		"javax.portlet.name=" + SearchBarPortletKeys.PORTLET_NAME,
+		"javax.portlet.resource-bundle=content.Language",
+		"javax.portlet.security-role-ref=guest,power-user,user",
+		"javax.portlet.supports.mime-type=text/html"
+	},
+	service = {Portlet.class, SearchAwarePortlet.class}
+)
+public class SearchBarPortlet extends MVCPortlet implements SearchAwarePortlet {
+
+	@Override
+	public void contribute(
+		PortletSharedSearchSettings portletSharedSearchSettings) {
+
+		SearchBarPortletPreferences searchBarPortletPreferences =
+			new SearchBarPortletPreferencesImpl(
+				portletSharedSearchSettings.getPortletPreferences());
+
+		filter(searchBarPortletPreferences, portletSharedSearchSettings);
+	}
+
+	@Override
+	public void render(
+			RenderRequest renderRequest, RenderResponse renderResponse)
+		throws IOException, PortletException {
+
+		PortletSharedSearchResponse portletSharedSearchResponse =
+			portletSharedSearchRequest.search(renderRequest);
+
+		Optional<String> keywordsOptional =
+			portletSharedSearchResponse.getKeywords();
+
+		SearchBarPortletPreferences searchBarPortletPreferences =
+			new SearchBarPortletPreferencesImpl(
+				Optional.ofNullable(renderRequest.getPreferences()));
+
+		// TODO Autocomplete
+
+		String fakeAutocompleteURL =
+			renderRequest.getContextPath() + "/search/portlet/bar/demo.json";
+
+		SearchBarPortletDisplayBuilder searchBarPortletDisplayBuilder =
+			new SearchBarPortletDisplayBuilder();
+
+		searchBarPortletDisplayBuilder.setAutocompleteURL(
+			searchBarPortletPreferences.getAutocompleteURL());
+		searchBarPortletDisplayBuilder.setKeywords(
+			keywordsOptional.orElse(StringPool.BLANK));
+		searchBarPortletDisplayBuilder.setKeywordsParameterName(
+			searchBarPortletPreferences.getKeywordsParameterNameString());
+		searchBarPortletDisplayBuilder.setScopePreference(
+			searchBarPortletPreferences.getScope());
+
+		// TODO Set / get Scope Selection from URL parameter
+
+		searchBarPortletDisplayBuilder.setScope(SearchScope.EVERYTHING);
+
+		// TODO see SearchDisplayContext (ThemeDisplay stagingGroup)
+
+		searchBarPortletDisplayBuilder.setScopePreferenceEverythingAvailable(
+			true);
+
+		SearchBarPortletDisplayContext searchBarPortletDisplayContext =
+			searchBarPortletDisplayBuilder.build();
+
+		renderRequest.setAttribute(
+			SearchBarPortletDisplayContext.ATTRIBUTE,
+			searchBarPortletDisplayContext);
+
+		super.render(renderRequest, renderResponse);
+	}
+
+	protected void filter(
+		SearchBarPortletPreferences searchBarPortletPreferences,
+		PortletSharedSearchSettings portletSharedSearchSettings) {
+
+		String paramName =
+			searchBarPortletPreferences.getKeywordsParameterNameString();
+
+		Optional<String> paramValueOptional =
+			portletSharedSearchSettings.getParameter(paramName);
+
+		paramValueOptional.ifPresent(portletSharedSearchSettings::setKeywords);
+	}
+
+	@Reference
+	protected PortletSharedSearchRequest portletSharedSearchRequest;
+
+}
