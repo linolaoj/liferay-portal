dceb2c2aeade0847aab7d57b3096bdcdc156b83d
André de Oliveira <andre.oliveira@liferay.com>
2016-09-07 16:51:23 -0700

    Extract minimal reusable Search from SearchDisplayContext

---
+++ modules/apps/foundation/portal-search/portal-search-web/src/main/java/com/liferay/portal/search/web/internal/display/context/SearchContainerSupplier.java
@@ -0,0 +1,9 @@
+package com.liferay.portal.search.web.internal.display.context;
+
+import com.liferay.portal.kernel.dao.search.SearchContainer;
+
+public interface SearchContainerSupplier {
+
+	public SearchContainer getSearchContainer();
+
+}
---
+++ modules/apps/foundation/portal-search/portal-search-web/src/main/java/com/liferay/portal/search/web/internal/display/context/SearchContextSupplier.java
@@ -0,0 +1,9 @@
+package com.liferay.portal.search.web.internal.display.context;
+
+import com.liferay.portal.kernel.search.SearchContext;
+
+public interface SearchContextSupplier {
+
+	public SearchContext getSearchContext();
+
+}
