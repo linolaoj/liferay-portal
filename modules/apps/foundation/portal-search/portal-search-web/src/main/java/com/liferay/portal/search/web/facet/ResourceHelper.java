82dde5e39d3d621767c6f5ba744a7c33a962aa72
André de Oliveira <andre.oliveira@liferay.com>
2016-09-28 15:06:34 -0700

    Reuse JSP Resource include logic between old and new Facets

---
+++ modules/apps/foundation/portal-search/portal-search-web/src/main/java/com/liferay/portal/search/web/facet/ResourceHelper.java
@@ -0,0 +1,41 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.search.web.facet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author André de Oliveira
+ */
+public class ResourceHelper {
+
+	public static void include(
+			String path, HttpServletRequest request,
+			HttpServletResponse response, ServletContext servletContext)
+		throws IOException, ServletException {
+
+		RequestDispatcher requestDispatcher =
+			servletContext.getRequestDispatcher(path);
+
+		requestDispatcher.include(request, response);
+	}
+
+}
