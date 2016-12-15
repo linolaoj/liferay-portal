1b1fa2f49c4003d97c5f734ed1d5768b356396f1
Lino Alves <lino.alves@liferay.com>
2016-12-15 17:36:03 -0300

    User Facet Portlet

---
+++ modules/apps/foundation/portal-search/portal-search-web/src/main/java/com/liferay/portal/search/web/internal/portlet/facet/user/UserFacetPortletPreferences.java
@@ -0,0 +1,44 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.search.web.internal.portlet.facet.user;
+
+/**
+ * @author Lino Alves
+ */
+public interface UserFacetPortletPreferences {
+
+	public static final boolean DEFAULT_FREQUENCIES_VISIBLE = true;
+
+	public static final String DEFAULT_PARAM_NAME = "user";
+
+	public static final String PREFERENCE_FREQUENCIES_VISIBLE =
+		"frequenciesVisible";
+
+	public static final String PREFERENCE_FREQUENCY_THRESHOLD =
+		"frequencyThreshold";
+
+	public static final String PREFERENCE_MAX_TERMS = "maxTerms";
+
+	public static final String PREFERENCE_PARAM_NAME = "paramName";
+
+	public int getFrequencyThreshold();
+
+	public int getMaxTerms();
+
+	public String getParamName();
+
+	public boolean isFrequenciesVisible();
+
+}
