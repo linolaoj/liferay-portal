393f1547916197857fff19e53d251fe1d719eb16
André de Oliveira <andre.oliveira@liferay.com>
2017-01-23 16:07:56 -0800

    Introducing Contributable Searches

---
+++ modules/apps/foundation/portal-search/portal-search-web/src/main/java/com/liferay/portal/search/web/search/SearchResultsData.java
@@ -0,0 +1,42 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.search.web.search;
+
+import aQute.bnd.annotation.ProviderType;
+
+import com.liferay.portal.kernel.search.Document;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * @author André de Oliveira
+ */
+@ProviderType
+public interface SearchResultsData {
+
+	public List<Document> getDocuments();
+
+	public String[] getHighlights();
+
+	public Optional<String> getKeywords();
+
+	public String getQueryString();
+
+	public int getStartPage();
+
+	public int getTotalHits();
+
+}
