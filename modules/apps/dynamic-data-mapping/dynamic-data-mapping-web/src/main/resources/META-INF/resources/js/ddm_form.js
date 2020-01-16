128f98b7c2e1e0c2790cdabad6ef4061335cffae
balazssk <balazs.safrany-kovalik@liferay.com>
2020-01-16 13:17:03 +0100

    LPS-106890 Use iteration instead of filter

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-web/src/main/resources/META-INF/resources/js/ddm_form.js
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-web/src/main/resources/META-INF/resources/js/ddm_form.js
@@ -3654,9 +3654,11 @@ AUI.add(
 
 					radioNodes.set('checked', false);
 
-					radioNodes
-						.filter('[value=' + value + ']')
-						.set('checked', true);
+					radioNodes.each(function(radioNode) {
+						if(radioNode.get('value') === value) {
+							radioNode.set('checked', true);
+						}
+					});
 				},
 
 				syncReadOnlyUI() {
